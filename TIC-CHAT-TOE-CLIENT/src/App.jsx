1f9f8f2a2f41cd643a70ee247b1a0231683a1c8d
Draako <adgutierrezdev@gmail.com>
2023-07-21 18:33:27 -0500

    hola

--- TIC-CHAT-TOE-CLIENT/src/App.jsx
+++ TIC-CHAT-TOE-CLIENT/src/App.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
 function App() {
+  const [hola, setHola] = useState("");
+
+  useEffect(() => {
+    socket.emit("pong", "Hola?");
+
+    socket.on("ping", ({ message }) => {
+      setHola(message);
+    });
+  }, []);
 
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <h1>Hola {hola}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
