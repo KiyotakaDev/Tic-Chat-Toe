12f2612f29386c8e429bc662f5fda6b6c8a28503
きよたか <c4mo.sw@gmail.com>
2023-07-22 14:46:51 -0500

    feat(controllers): Creating the register controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,10 +1,36 @@
 // Importing User model
 import User from "../models/auth.model.js";
+import bcryptjs from "bcryptjs";
 
 // Creating register controller
 export const register = async (req, res) => {
   try {
-    console.log(req.body);
+    // Deconstruction of req.body
+    const { username, email, password } = req.body;
+
+    // Validating if the user doesn't exist already
+    const userFound = await User.findOne({ email });
+    if (userFound) return res.status(409).json(["The email is already in use"]);
+
+    // Creating a passwordhash
+    const passwordHash = await bcryptjs.hash(password, 12);
+
+    // Creting new user with User model and saving it
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
+    const userSaved = await newUser.save();
+
+    // Returning user data
+    return res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
     return res.status(500).json({ error });
   }
