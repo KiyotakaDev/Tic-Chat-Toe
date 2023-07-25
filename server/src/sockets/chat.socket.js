b41c8d81236238736ea3b900ee9dda1f892a8dd6
きよたか <c4mo.sw@gmail.com>
2023-07-25 14:44:31 -0500

    feat(chat.socket): Adding basic socket.io code for sending the message

--- server/src/sockets/chat.socket.js
+++ server/src/sockets/chat.socket.js
@@ -1,10 +1,36 @@
+// Importing message controllers
+import {
+  createMessage,
+  getMessages,
+  getMessageByKeyword,
+  updateMessage,
+  deleteMessage,
+} from "../controllers/message.controller.js";
+
 // Basic socket communication
 const chatSocket = (io) => {
   io.on("connect", (socket) => {
-    socket.emit("ping", { message: "ping" });
-    socket.on("pong", (data) => {
-      console.log(data);
-    });
+    console.log("Client connected", socket.id);
+
+    // Assigning id for user when registered or logged
+    socket.on("setUserId", (userId) => {
+      socket.userId = userId
+    })
+
+    socket.on('sendmessage', async ({ message, date }) => {
+      try {
+        // Creating the message
+        const newMessage = await createMessage({
+          message,
+          date,
+          userId: socket.userId
+        })
+        // Sending message
+        io.emit("recievedMessage", newMessage)
+      } catch (error) {
+        console.error("Saving message error", error);
+      }
+    })
   });
 };
 
